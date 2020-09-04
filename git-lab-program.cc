ff5ca030d25a74a96276728b6882cbb19cf9af3d
Leah Wagner <70283446+leahwag@users.noreply.github.com>
2020-09-03 20:09:38 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Leah Wagner
+ *        Date: September 3, 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
